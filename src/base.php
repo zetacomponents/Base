f07067c240b61510cc7b1d7bd437168c6c1bf2d7
Benjamin Eberlei <kontakt@beberlei.de>
2022-01-24 13:35:04 +0100

    Remove extra whitespaces at the end of lines.

--- src/base.php
+++ src/base.php
@@ -9,9 +9,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -576,7 +576,6 @@ class ezcBase
         {
             $autoloadDirPath = $basePath . '/autoload';
         }
-
 
         // check if autoload dir exists
         if ( !is_dir( $autoloadDirPath ) )
